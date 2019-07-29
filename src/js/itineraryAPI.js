f6d833e2e6878f3087961f147f4de7a382ddadb1
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-29 16:07:58 -0500

    Added functionality to update json server to provide dynamic data instead of hard coded json objects

--- src/js/itineraryAPI.js
+++ src/js/itineraryAPI.js
@@ -1,9 +1,47 @@
 const concertSpan = document.querySelector("#savedConcert__span")
 
-fetch(`http://localhost:8088/itinerary`) 
-    .then(itinerary => itinerary.json()) 
-    .then(parsedItinerary => {
-        console.log(parsedItinerary)
-        concertSpan.innerHTML = parsedItinerary.concert
-    })
+const getItinerary = () => {
+    fetch(`http://localhost:8088/itinerary`)
+        .then(itinerary => itinerary.json())
+        .then(parsedItinerary => {
+            concertSpan.innerHTML = parsedItinerary.concert
+        })
+}
+
+const postOrPutItinerary = (url, data) =>
+    fetch(url)
+        .then(itinerary => itinerary.json())
+        .then(parsedItinerary => {
+            // IF the data doesn't exist then POST it
+            if (!parsedItinerary.hasOwnProperty("id")) {
+                return fetch(url, {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    body: JSON.stringify(data),
+                })
+                    .then(response => response.json()) // parses JSON response into native JavaScript objects 
+            } else {
+                // ELSE use PUT Method to update the itinerary
+                return fetch(url, {
+                    method: "PUT",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        // 'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    body: JSON.stringify(data),
+                })
+                    .then(response => response.json()) // parses JSON response into native JavaScript objects 
+            }
+        })
+        .then(() => {
+            getItinerary()
+        })
+
+
+
+
+
 
