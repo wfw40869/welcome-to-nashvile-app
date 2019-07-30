6fd7a8621555bdaab6a4b26e723bf7cc6bab748b
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-29 21:38:37 -0500

    Finished styling buttons and basic layout of sections

--- src/js/render-results.js
+++ src/js/render-results.js
@@ -4,7 +4,7 @@ const renderResults = (concertObj) => {
             <p class="concert__name">${concertObj.name}</p>
             <p>Date: ${concertObj.dates.start.localDate}</p>
             <p>Location: <span class="concert__location">${concertObj._embedded.venues[0].name}</span></p>
-            <a href="${concertObj.url}" class="tickets__button" target="_blank">But Tickets</a>
+            <a href="${concertObj.url}" class="tickets__button" target="_blank">Buy Tickets</a>
             <button class="save__button">Save to Itinerary</button>
         </li>
     `
