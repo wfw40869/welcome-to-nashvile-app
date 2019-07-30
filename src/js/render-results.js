d8587e35f31e75bc87954f15f56e90badd264047
Will Wilkinson <40211746+wfw40869@users.noreply.github.com>
2019-07-29 22:50:19 -0500

    Finished styling page for desktop use.

--- src/js/render-results.js
+++ src/js/render-results.js
@@ -1,9 +1,9 @@
 const renderResults = (concertObj) => {
     return `
         <li>
-            <p class="concert__name">${concertObj.name}</p>
-            <p>Date: ${concertObj.dates.start.localDate}</p>
-            <p>Location: <span class="concert__location">${concertObj._embedded.venues[0].name}</span></p>
+            <h3 class="concert__name">${concertObj.name}</h3>
+            <p>Date: <span class="date__span">${concertObj.dates.start.localDate}</span></p>
+            <p>Location: <span class="location__span">${concertObj._embedded.venues[0].name}</span></p>
             <a href="${concertObj.url}" class="tickets__button" target="_blank">Buy Tickets</a>
             <button class="save__button">Save to Itinerary</button>
         </li>
